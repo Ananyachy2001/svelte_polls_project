97ffa161784436d2ae8e8ea0e0154477632762b0
Ananyachy2001 <ananyachy2001@gmail.com>
2024-06-19 17:15:33 +0600

    header and footer

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Header from './components/Header.svelte';
+	import Footer from './components/Footer.svelte';
+
+</script>
+
+<Header />
+<main>
+	
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, rem quibusdam? Ullam non nostrum voluptas ex consequatur officia rerum? Voluptates beatae nostrum ex consequuntur asperiores quibusdam alias sit accusamus illum!</p>
+</main>
+<Footer/>
+
+<style>
+main{
+	max-width: 950px;
+	margin: 40px auto;
+}
+</style>
