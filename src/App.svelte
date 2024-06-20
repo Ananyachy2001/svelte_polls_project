2423173cea4f14995e865f2d926b307099c8f54a
Ananyachy2001 <ananyachy2001@gmail.com>
2024-06-20 09:42:52 +0600

    adding new poll and details component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+	import PollList from './components/PollList.svelte';
 	import CreatePollForm from './components/CreatePollForm.svelte';
 	import Tabs from './shared/Tabs.svelte';
 	
@@ -11,15 +12,36 @@
 	const tabChange = (e) =>{
 		activeItem = e.detail;
 	}
+	//polls
+	let polls = [
+		{
+			id: 1,
+			question: 'Python or Javascript',
+			answerA: 'Python',
+			answerB: 'JavaScript',
+			votesA: 9,
+			votesB: 15,
+		},
+	];
+
+
+	const handleAdd = (e) => {
+		const poll = e.detail;
+		polls = [poll, ...polls];
+		console.log(polls);
+		activeItem = "Current Polls"
+	}
+
+
 </script>
 
 <Header />
 <main>
 	<Tabs {activeItem}  {items} on:tabChange={tabChange} />
 	{#if activeItem === 'Current Polls'}
-	<p>Poll List Component goes here</p>
+	<PollList {polls}/>
 	{:else if activeItem === "Add New Poll"}
-	<CreatePollForm />
+	<CreatePollForm on:add={handleAdd}/>
 	{/if}
 </main>
 <Footer/>
